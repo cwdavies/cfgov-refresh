7cee9c2b40ca97874c6c76c6c463d79a3ae18d3d
el-mapache <adam.biagianti@gmail.com>
2019-09-03 08:42:56 -0700

    Hides checkboxes in a no-js environment

--- cfgov/unprocessed/apps/youth-employment-success/js/index.js
+++ cfgov/unprocessed/apps/youth-employment-success/js/index.js
@@ -6,7 +6,6 @@ import routeOptionFormView from './route-option-view';
 import routeOptionToggleView from './route-option-toggle-view';
 import store from './store';
 
-
 const BUDGET_CLASSES = budgetFormView.CLASSES;
 const OPTION_CLASSES = routeOptionFormView.CLASSES;
 const OPTION_TOGGLE_CLASSES = routeOptionToggleView.CLASSES;
@@ -24,7 +23,7 @@ const routeOptionForms = expandables.map( ( expandable, index ) => {
 } );
 
 /* only initialize the first form, the other gets initialized when
-   the user clicks 'add another option' button */
+  the user clicks 'add another option' button */
 routeOptionForms[0].init();
 routeOptionToggleView(
   document.querySelector( `.${ OPTION_TOGGLE_CLASSES.BUTTON }` ), {
